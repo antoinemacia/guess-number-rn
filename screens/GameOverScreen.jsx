85b70000e779e3b912ab2c93c3ab2376e6c36a7b
Antoine Macia <antoine.macia83@gmail.com>
2019-12-24 10:37:03 +1100

    Add loading of images (over local & network)

--- screens/GameOverScreen.jsx
+++ screens/GameOverScreen.jsx
@@ -7,7 +7,16 @@ const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
       <BodyText>The Game is Over!</BodyText>
-      <Image source={}/>
+      {
+        // Loading local images using the above syntax
+        // <Image source={require('../assets/images/success.png')}/>
+        // From web:
+        // <Image source={uri: 'https://image.com'}/>
+      }
+      <Image
+        source={require('../assets/images/success.png')}
+        style={styles.image}
+        resizeMode="cover"/>
       <BodyText>Number of Rounds: {props.roundsNumber}</BodyText>
       <BodyText>Number was: {props.userNumber}</BodyText>
       <View style={styles.buttonContainer}>
@@ -30,6 +39,15 @@ const styles = StyleSheet.create({
     marginTop: 20,
     width: 300,
     maxWidth: '80%'
+  },
+  image: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: 'black',
+    overflow: 'hidden',
+    marginVertical: 30
   }
 });
 
